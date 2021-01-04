34558066ec82ba1f9b46fd677718008c2d7f2c82
tharkne <tharkne@umich.edu>
2021-01-04 00:27:06 -0500

    Add skeleton Nav component

---
+++ src/components/BigNav.js
@@ -0,0 +1,8 @@
+// Top Nav bar component
+import { makeStyles, Grid } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+
+export default function BigNav(props){
+    <Typography>BigNav</Typography>
+}
